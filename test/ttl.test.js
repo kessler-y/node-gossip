bc81fb5331d7bf0dc53c38cd569c47f29d919aad
Yaniv Kessler <yanivk@gmail.com>
2014-02-17 23:07:30 +0200

    added started event

--- test/ttl.test.js
+++ test/ttl.test.js
@@ -23,16 +23,16 @@ function listenToGossip(gossiper, listenCount, cb) {
 
 module.exports = {
 	'ttl should propagate to other peers and get expired throughout the net': function(beforeExit, assert) {
-		var seed = new Gossiper(PORT1, [], '127.0.0.1');			
-		var g1 = new Gossiper(PORT2, ['127.0.0.1:' + PORT1], '127.0.0.1');			
+		var seed = new Gossiper(PORT1, [], '127.0.0.1');
+		var g1 = new Gossiper(PORT2, ['127.0.0.1:' + PORT1], '127.0.0.1');
 		var g2 = new Gossiper(PORT3, ['127.0.0.1:' + PORT1], '127.0.0.1');
 		var testTTL = Date.now() + 8000;
 
 		var expiredCount = 0;
-		
+
 		function checkExpired(expired) {
 			assert.ok('x' in expired);
-			expiredCount++;		
+			expiredCount++;
 			console.log('expired fired');
 		}
 
@@ -40,22 +40,28 @@ module.exports = {
 		g2.on('expired', checkExpired);
 		seed.on('expired', checkExpired);
 
-		seed.start(function() {				
+		seed.start(function() {
 			console.log('seed started');
-			g1.start(function() {
-				console.log('g1 started');
-				g2.start(function() {
-					console.log('g2 started');
-					g2.setLocalState('x', 2512, testTTL);					
-					
-					listenToGossip(g1, 1);
-					listenToGossip(g2, 1);
-					listenToGossip(seed, 1);
+			setTimeout(function() {
+				g1.start(function() {
+					console.log('g1 started');
 
-					console.log('waiting 5 seconds for data to propagated');
-					setTimeout(goPhase1, 5000);
-				});					
-			});
+					setTimeout(function () {
+						g2.start(function() {
+							console.log('g2 started');
+							g2.setLocalState('x', 2512, testTTL);
+
+							listenToGossip(g1, 1);
+							listenToGossip(g2, 1);
+							listenToGossip(seed, 1);
+
+							console.log('waiting 5 seconds for data to propagate');
+							setTimeout(goPhase1, 5000);
+						});
+					}, 1000)
+
+				});
+			}, 1000);
 		});
 
 		function goPhase1() {
@@ -65,7 +71,7 @@ module.exports = {
 			var now = Date.now();
 
 			assert.ok(testTTL > now, 'ttl should be greater than current now');
-			
+
 			assert.ok('x' in g2.my_state.attrs, 'g2 didnt include x');
 			assert.strictEqual(testTTL, g2.my_state.attrs['x'][2]);
 
@@ -76,18 +82,18 @@ module.exports = {
 			assert.strictEqual(testTTL, seed.my_state.attrs['x'][2]);
 
 			console.log('state and ttl was ok');
-			goPhase2();			
+			goPhase2();
 		}
 
-		function goPhase2() {			
+		function goPhase2() {
 			console.log('waiting for keys to expire');
 			setTimeout(function() {
-					
+
 				assert.ok(!('x' in g2.my_state.attrs), 'g2 include x when it shouldnt');
-				
+
 				assert.ok(!('x' in g1.my_state.attrs), 'g1 include x when it shouldnt');
-				
-				assert.ok(!('x' in seed.my_state.attrs), 'seed include x when it shouldnt');				
+
+				assert.ok(!('x' in seed.my_state.attrs), 'seed include x when it shouldnt');
 
 				console.log('waiting 10 more seconds to check if "x" was removed from peer mirrored state in other peers');
 				setTimeout(function() {
